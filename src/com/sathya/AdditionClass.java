42253dcaf9265f5978490021a9a13a8fe53f7466
Pranaybaharghare <118751225+Pranaybaharghare@users.noreply.github.com>
2023-05-31 15:10:05 +0530

    modification done

--- src/com/sathya/AdditionClass.java
+++ src/com/sathya/AdditionClass.java
@@ -6,7 +6,7 @@ public class AdditionClass {
 		int num1 = 10;
 		int num2 = 20;
 		int result;
-		result = num1 + num2;
+		result = num1 * num2;
 		System.out.println("addition of two numbers: " + result);
 	}
 
---
+++ src/com/sathya/SubstractionClass.java
@@ -0,0 +1,13 @@
+package com.sathya;
+
+public class SubstractionClass {
+
+	public static void main(String[] args) {
+int n1=40;
+int n2=20;
+int result;
+result=n1-n2;
+System.out.println("substraction of two numbers: "+result);
+	}
+
+}
