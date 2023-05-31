c2975ea91e218a62db58fc0a1b9892ffdba2931c
Pranaybaharghare <118751225+Pranaybaharghare@users.noreply.github.com>
2023-05-31 14:55:33 +0530

    addition complete

---
+++ src/com/sathya/AdditionClass.java
@@ -0,0 +1,13 @@
+package com.sathya;
+
+public class AdditionClass {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int result;
+		result = num1 + num2;
+		System.out.println("addition of two numbers: " + result);
+	}
+
+}
